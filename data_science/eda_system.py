3d334c9c61e3fe38610d4958d3b8959aabbad88c
Tiago Costa <feitosadacosta.tiago@gmail.com>
2026-05-22 11:12:11 -0400

    switched manual standart deviation and mean with describe() function

--- data_science/eda_system.py
+++ data_science/eda_system.py
@@ -18,11 +18,7 @@ df = pd.read_csv(file_path)
 #print(df.head()) #head retorna as primeiras n linhas passadas como argumento. Por padrão, retorna as 5 primeiras
 
 #Gerando estatísticas descritivas
-media = np.mean(df["Crude_Oil_Price"])
-desvio_padrao = np.std(df['Crude_Oil_Price'])
-
-print(round(media, 2))
-print(round(desvio_padrao, 2))
+print(df.describe()) #Mostra algumas estatísticas descritivas relativas ao dataset
 
 #Criando o gráfico
 ax = df.plot(x='Date', y='Crude_Oil_Price', title='Preço do petróleo com o passar dos anos')
